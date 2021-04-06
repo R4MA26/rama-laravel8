51a16ff782a3afb74253c7c47311dcccf2fca968
R4MA26 <ramaismaya25@gmail.com>
2021-04-06 16:54:31 +0800

    rama-laravel8

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use app\Http\Controllers\TestController;
+
+
+
+class TestController extends Controller
+{
+    //
+    public function index(){
+    	return "ok bro...";
+    }
+}
